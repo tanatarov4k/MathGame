380dfc129784a8f52df373dd825061b80590b0c0
Koshkar Tanatarov <tanatarov4k@gmail.com>
2020-07-22 22:14:15 +0500

    added additional func

--- app/src/main/java/com/example/mthgame/awa.kt
+++ app/src/main/java/com/example/mthgame/awa.kt
@@ -9,6 +9,9 @@ class awa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_awa)
+        var wrongJuwaplarSani = intent.getIntExtra(ush.WRONG_ANSWERS_COUNT,0)
+        var rightJuwaplarSani = intent.getIntExtra(ush.RIGHT_ANSWERS_COUNT,0)
+        text1234.text = "Duris juwap: $rightJuwaplarSani\nQa'te juwap: $wrongJuwaplarSani"
         buttonAwa.setOnClickListener {
             val intento = Intent(this,eki::class.java)
             intento.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
